48941d48986ec74f72e48565e9456359be4a1fe3
Sunny Yan <sunny.yan@sunnyyan.com>
2024-04-06 22:38:25 +0800

    WIP

--- advenv/bin/sender.sh
+++ advenv/bin/sender.sh
@@ -8,7 +8,7 @@ file_list=file_list.txt
 mkdir $tmpfolder
 
 while read line; do
-    cp -p ${line} ${tmpfolder}
+    cp -p "${line}" ${tmpfolder}
 done < $file_list
 
 tar Jcf - ${tmpfolder} | base64 > ${b64txt}
