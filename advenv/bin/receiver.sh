cb9e1de12a4832cb8724f75ae52f0163735e3a99
Sunny Yan <sunny.yan@sunnyyan.com>
2024-04-10 13:55:41 +0800

    WIP

--- advenv/bin/receiver.sh
+++ advenv/bin/receiver.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 tmpfile=$(mktemp)
-vim $tmpfile
+
+if [[ $# -eq 0 ]]
+then
+    vim $tmpfile
+else
+    cat $1 > $tmpfile
+fi
 
 cat $tmpfile | ./encrypt.py d | base64 --decode | tar Jxf -
+
 rm -f $tmpfile
