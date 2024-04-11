84bc472f3c5489e6c0def6645b016bf33ffcf297
Sunny Yan <sunny.yan@sunnyyan.com>
2024-04-11 17:51:47 +0800

    WIP

--- advenv/bin/update_dbx.sh
+++ advenv/bin/update_dbx.sh
@@ -9,6 +9,21 @@ fi
 folder=$1
 for f in $folder/*
 do
-    echo $f
-    vimdiff $f $(cat file_list.txt | grep $(basename $f))
+    file1=$f
+    file2=$(cat file_list.txt | grep $(basename $f))
+    echo
+    md5sum $file1 $file2
+    echo
+    if [[ $(md5sum $file1 | awk '{print $1}') != $(md5sum $file1 | awk '{print $1}') ]]
+    then
+        echo "Overwrite with $file1 ? [yes|N]"
+        read user_input
+        if [[ $user_input == "yes" || $user_input == "YES" ]]
+        then
+            cat $file1 > $file2
+            echo "$file2 was Overwritten"
+            md5sum $file1 $file2
+        fi
+    fi
+    vimdiff $file1 $file2
 done
